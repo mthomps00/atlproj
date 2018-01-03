bdbdd9421147179fd8d9e8974e7ea8629cfe8095
Matt Thompson <service@emailmatt.com>
2018-01-03 01:35:23 +0000

    Cleaned up views and tweaked front page index

--- atlproj/ideas/urls.py
+++ atlproj/ideas/urls.py
@@ -16,16 +16,9 @@ urlpatterns = [
         url(r'^(?P<selector>[\w]+)/$', views.ideas_by_status, name='ideas_by_status_flattened'),
         url(r'^(?P<selector>[\w]+)/(?P<platform>[\w]+)/$', views.ideas_by_status, name='ideas_by_status_flattened'),
         ]), { 'flatten' : True, }),
-    url(r'^available/', include([
-        url(r'^$', IdeasList.as_view(), name='idea_list'),
-        url(r'^(?P<platform>[\w]+)/$', PlatformIdeasList.as_view(), name='platform_idea_list'),
-        ])),
-    url(r'^active/', include([
-        url(r'^$', CurrentIdeas.as_view(), name='current_list'),
-        url(r'^(?P<platform>[\w]+)/$', PlatformCurrentIdeas.as_view(), name='platform_current_list'),
-        ])),
-    url(r'^complete/', include([
-        url(r'^$', CompletedIdeas.as_view(), name='complete_list'),
+    url(r'^calendar/', include([
+        url(r'^$', views.ideas_by_date, name='calendar'),
+        url(r'^(?P<platform>[\w]+)/$', views.ideas_by_date, name='platform_calendar'),
         ])),
     url(r'^detail/', include([
         url(r'^(?P<pk>[0-9]+)/$', IdeaDetail.as_view(), name='idea_detail'),
@@ -33,10 +26,6 @@ urlpatterns = [
         ])),
     url(r'^tag/', include([
         url(r'^(?P<pk>[0-9]+)/$', TagDetail.as_view(), name='tag_detail'),
-        ])),
-    url(r'^calendar/', include([
-        url(r'^$', IdeasCalendar.as_view(), name='calendar'),
-        url(r'^calendar/(?P<platform>[\w]+)/$', PlatformCalendar.as_view(), name='platform_calendar'),
         ])),
     url(r'^pitches/', include([
         url(r'^$', PitchList.as_view(), name='pitch_list'),
--- atlproj/ideas/views.py
+++ atlproj/ideas/views.py
@@ -105,19 +105,31 @@ def ideas_by_status(request, selector='all', platform=False, flatten=False):
         'platform': platform,
         'display_name': display_name,
         'selector': selector,
-        'statuses': list(status),
+        'statuses': sorted(list(status)),
         'flatten': flatten,
     }
     return render(request, 'ideas/ideas_flexlist.html', context)
+
+@login_required
+def ideas_by_date(request, platform=False):
+    today = datetime.today()
+    ideas = Idea.objects.filter(start_date__gte=today).order_by('start_date')
+    
+    if platform:
+        platform = get_object_or_404(Platform, name=platform)
+        ideas = ideas.filter(platform=platform)
+
+    context = {
+        'object_list': ideas,
+        'platform': platform,
+    }
+    
+    return render(request, 'ideas/calendar.html', context)
 
 #####################
 # CLASS-BASED VIEWS #
 #####################
 
-class IdeasList(LoginRequiredMixin, ListView):
-    queryset = Idea.objects.filter(parent__isnull=True).filter(status='ON_OFFER').order_by('start_date', '-date_updated')
-    context_object_name = 'object_list'
-    
 class PitchList(LoginRequiredMixin, ListView):
     model = Pitch
     context_object_name = 'pitches'
@@ -132,88 +144,10 @@ class SoldPitches(LoginRequiredMixin, ListView):
         context['sold'] = True
         return context
 
-class CurrentIdeas(LoginRequiredMixin, ListView):
-    today = datetime.today()
-    queryset = Idea.objects.filter(parent__isnull=True).filter(status='LIVE').exclude(end_date__lte=today).order_by('start_date', '-date_updated')
-    context_object_name = 'object_list'
-
-    def get_context_data(self, **kwargs):
-        context = super(CurrentIdeas, self).get_context_data(**kwargs)
-        context['current'] = True
-        return context
-    
-class CompletedIdeas(LoginRequiredMixin, ListView):
-    today = datetime.today()
-    queryset = Idea.objects.filter(parent__isnull=True).filter(status='COMPLETED').order_by('-end_date')
-    context_object_name = 'object_list'
-
-    def get_context_data(self, **kwargs):
-        context = super(CompletedIdeas, self).get_context_data(**kwargs)
-        context['complete'] = True
-        return context
-    
-class AllIdeas(LoginRequiredMixin, ListView):
-    today = datetime.today()
-    queryset = Idea.objects.all()
-    context_object_name = 'object_list'
-
-    def get_context_data(self, **kwargs):
-        context = super(AllIdeas, self).get_context_data(**kwargs)
-        context['complete'] = True
-        return context
-    
 class IdeaRetire(LoginRequiredMixin, ListView):
     today = datetime.today()
     queryset = Idea.objects.filter(status='LIVE').filter(end_date__lte=today).order_by('end_date')
     context_object_name = 'object_list'
-
-class IdeasCalendar(LoginRequiredMixin, ListView):
-    today = datetime.today()
-    queryset = Idea.objects.filter(start_date__gte=today).order_by('start_date')
-    template_name = 'ideas/calendar.html'
-    context_object_name = 'object_list'
-
-class PlatformCalendar(LoginRequiredMixin, ListView):
-    template_name = 'ideas/calendar.html'
-    context_object_name = 'object_list'
-    
-    def get_queryset(self):
-        today = datetime.today()
-        self.platform = get_object_or_404(Platform, name=self.kwargs['platform'])
-        return Idea.objects.filter(start_date__gte=today).order_by('start_date').filter(platform=self.platform)
-        
-    def get_context_data(self, **kwargs):
-        context = super(PlatformCalendar, self).get_context_data(**kwargs)
-        context['platform'] = self.platform
-        return context
-
-class PlatformIdeasList(LoginRequiredMixin, ListView):
-    template_name = 'ideas/idea_list.html'
-    context_object_name = 'object_list'
-    
-    def get_queryset(self):
-        self.platform = get_object_or_404(Platform, name=self.kwargs['platform'])
-        return Idea.objects.filter(status='ON_OFFER').order_by('-start_date', '-date_updated').filter(platform=self.platform)
-        
-    def get_context_data(self, **kwargs):
-        context = super(PlatformIdeasList, self).get_context_data(**kwargs)
-        context['platform'] = self.platform
-        return context
-
-class PlatformCurrentIdeas(LoginRequiredMixin, ListView):
-    template_name = 'ideas/idea_list.html'
-    context_object_name = 'object_list'
-    
-    def get_queryset(self):
-        today = datetime.today()
-        self.platform = get_object_or_404(Platform, name=self.kwargs['platform'])
-        return Idea.objects.filter(status='LIVE').exclude(end_date__lte=today).order_by('-start_date', '-date_updated').filter(platform=self.platform)
-        
-    def get_context_data(self, **kwargs):
-        context = super(PlatformCurrentIdeas, self).get_context_data(**kwargs)
-        context['platform'] = self.platform
-        context['current'] = True
-        return context
 
 class IdeaDetail(LoginRequiredMixin, DetailView):
     model = Idea
